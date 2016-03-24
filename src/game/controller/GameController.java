7dcef7fe8317de9dd3ce442d58cfc95c97e1b5b4
denonbarton <dbar0540@740s-pl-126.local>
2016-03-24 13:54:05 -0600

    1213

--- src/game/controller/GameController.java
+++ src/game/controller/GameController.java
@@ -7,28 +7,61 @@ public class GameController
 {
 	public static void main(String args[])
 	{
-		Scanner gameInput = new Scanner(System.in);
+		
+		Scanner wantToPlay = new Scanner(System.in);
+		String userResponse;
+		String userResponseAnswer;
+		
+		System.out.println(" Do you want to play Rock Paper Scissors(y/n)?");
+		userResponse = wantToPlay.next();
+		
+		if(userResponse.equals("y"))
+		{
+			userResponse = "Okay lets play";
+		}
+		else
+		{
+			System.exit(0);
+		}
+		
+		System.out.println("Rock(1) Paper(2) or Scissors(3)? : ");
+		
+		String userAnswer = null;
 		String answer;
 		Random botRandom = new Random();
-		String one;
-		String two;
-		String three;
 		
-		
+		Scanner gameInput = new Scanner(System.in);
 
-		System.out.println("lets play Rock Paper Scissors");
-		System.out.println("Rock(one) Paper(two) or Scissors(three)? : ");
+		System.out.println("Rock(1) Paper(2) or Scissors(3)? : ");
 		answer = gameInput.next();
-		if (answer == "one") answer = "Rock";
-		if (answer == "two") answer = "Paper";
-		if (answer == "three") answer = "scissors";
-		System.out.println("you picked " + answer);
 		
 		
 		
-		
-	}
+		System.out.println("Rock(1) Paper(2) or Scissors(3)? : ");
+		answer = gameInput.next();
+
+		if (answer.equals("1"))
+		{
+			userAnswer = "Rock";
+		}
+		else if (answer.equals("2"))
+		{
+			userAnswer = "Paper";
+		}
+		else if (answer.equals("3"))
+		{
+			userAnswer = "scissors";
+		}
+		else
+		{
+			System.out.println("Please pick again");
+		}
+				
+		System.out.println("you picked " + userAnswer);
+}
+
 	
+
 	public void start()
 	{
 		// TODO Auto-generated method stub
