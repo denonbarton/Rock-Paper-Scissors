270dfb69a9a8a6efb26bc719a3e5b8e15c8aba80
denonbarton <dbar0540@740s-pl-126.local>
2016-05-12 11:58:36 -0600

    Finalized Project

--- src/game/controller/GameController.java
+++ src/game/controller/GameController.java
@@ -1,66 +1,110 @@
 package game.controller;
 
+import java.util.Scanner;
+import java.math.*;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Set;
 
-public class GameController
+public class GameController 
 {
 	public static void main(String args[])
 	{
-		
-		Scanner wantToPlay = new Scanner(System.in);
-		String userResponse;
-		String userResponseAnswer;
-		
-		System.out.println(" Do you want to play Rock Paper Scissors(y/n)?");
-		userResponse = wantToPlay.next();
-		
-		if(userResponse.equals("y"))
-		{
-			userResponse = "Okay lets play";
-		}
-		else
-		{
-			System.exit(0);
-		}
-		
-		System.out.println("Rock(1) Paper(2) or Scissors(3)? : ");
-		
-		String userAnswer = null;
-		String answer;
-		Random botRandom = new Random();
-		
 		Scanner gameInput = new Scanner(System.in);
+		String answer;
+		double random;
+		String botPick;
+		String cpuAnswer;
+		int i;
 
-		System.out.println("Rock(1) Paper(2) or Scissors(3)? : ");
+		System.out.println("lets play Rock Paper Scissors");
+		System.out.println("(Rock) (Paper) or (Scissors)? : ");
 		answer = gameInput.next();
 		
 		
+//		for( i = 0; i < 5; i++)
+//		{
+//			if(answer != "Rock" || answer != "Scissors" || answer != "Paper")
+//			{
+//				System.out.println("Please try again the options are (Rock) (Paper) or (Scissors). : ");
+//				answer = gameInput.next();
+//				
+//			}
+//			else
+//			{
+//				i = 6;
+//			}
+//		}
+//		
 		
-		System.out.println("Rock(1) Paper(2) or Scissors(3)? : ");
-		answer = gameInput.next();
-
-		if (answer.equals("1"))
+		
+		System.out.println("You picked " + answer);
+		random = Math.random();
+		//System.out.println(random);
+		
+		if(random <= .33)
 		{
-			userAnswer = "Rock";
+			 cpuAnswer = "Paper";
 		}
-		else if (answer.equals("2"))
+		else if (random >= .34 && random <= .66)
 		{
-			userAnswer = "Paper";
-		}
-		else if (answer.equals("3"))
-		{
-			userAnswer = "scissors";
+			cpuAnswer = "Scissors";
 		}
 		else
 		{
-			System.out.println("Please pick again");
+			cpuAnswer = "Rock";
 		}
-				
-		System.out.println("you picked " + userAnswer);
-}
+		
+		System.out.println("The computer Picked " + cpuAnswer);
 
-	
+		if(answer.equals("Paper"))
+		{
+			if(cpuAnswer.equals("Paper"))
+			{
+				System.out.println("Looks like its a tie!");
+			}
+			else if(cpuAnswer.equals("Scissors"))
+			{
+				System.out.println("Ouch, you lost");
+			}
+			else
+			{
+				System.out.println("Nice job, you won!");
+			}
+		}
+		else if(answer.equals("Scissors"))
+		{
+			if(cpuAnswer.equals("Paper"))
+			{
+				System.out.println("Nice job, you won!");
+			}
+			else if(cpuAnswer.equals("Scissors"))
+			{
+				System.out.println("Looks like its a tie!");
+			}
+			else
+			{
+				System.out.println("Ouch, you lost");
+			}
+		}
+		else //(answer.equals("Rock"))
+		{
+			if(cpuAnswer.equals("Paper"))
+			{
+				System.out.println("Ouch, you lost");
+			}
+			else if(cpuAnswer.equals("Scissors"))
+			{
+				System.out.println("Nice job, you won!");
+			}
+			else
+			{
+				System.out.println("Looks like its a tie!");
+			}
+		}
+
+	}
+
 
 	public void start()
 	{
